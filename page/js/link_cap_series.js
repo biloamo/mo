16b43826ef3e0c3537918d5d3006e74b9adf9da3
biloamo <137099775+biloamo@users.noreply.github.com>
2023-08-29 15:24:07 -0500

    Add files via upload

---
+++ page/js/link_cap_series.js
@@ -0,0 +1,47 @@
+//next
+const web_S001 =[
+
+  "https://biloa.website/biloa/AWqER5VxVnHMiOA",
+  "https://biloa.website/biloa/Ixo1YzpWGRwc3M4",
+  "https://biloa.website/biloa/PUXf3001quu5Lhl",
+  "https://biloa.website/biloa/l547GZugpkLFyPL",
+  "https://biloa.website/biloa/DUHAtHmRG8nmqOx",
+  "https://biloa.website/biloa/b0qBvhi1Go6zUbS",
+  "https://biloa.website/biloa/X3BP42TCHcWhaHD",
+  "https://biloa.website/biloa/xnbgm9z7yGbjXKD"
+  
+  ]
+  const app_S001 =[
+
+  "https://mo.biloa.website/biloa/res9aOLP31zELQP",
+  "https://mo.biloa.website/biloa/9yYOy18Se0iJgWg",
+  "https://mo.biloa.website/biloa/cYZ9hu2gDUtNhcz",
+  "https://mo.biloa.website/biloa/cwecaUcgAXNFfbV",
+  "https://mo.biloa.website/biloa/s678xWoYgdPEyvp",
+  "https://mo.biloa.website/biloa/0IuxPynDDN72xIE",
+  "https://mo.biloa.website/biloa/7AGG9Y5s3Tj4Rfp",
+  "https://mo.biloa.website/biloa/8lTYXSBKo7zSaII"
+    
+  ]
+  const des_S001 =[
+
+  "https://drive.google.com/u/6/uc?id=1o-ocBcRaugFaxKacw8mmE-U621YZgxqf&export=download",
+  "https://drive.google.com/u/6/uc?id=1LIcn35ijm98m0toWrKHEXdTNbDqBn2Ov&export=download",
+  "https://drive.google.com/u/6/uc?id=17Ag9jjK_jKVlRwQWhHN1CiPk3gWHEmw5&export=download",
+  "https://drive.google.com/u/6/uc?id=1vseQWWxI45IOgYz9lGCtuMxPTjwS6oy3&export=download",
+  "https://drive.google.com/u/6/uc?id=1ePDxv8o9pL3v76tlvNMqpwW-fCC2QICj&export=download",
+  "https://drive.google.com/u/6/uc?id=127DIMvzIhoQNvBMP-RZFlT9LbzqVCRK9&export=download",
+  "https://drive.google.com/u/6/uc?id=1oS-tCxSv9bFVw45SNtcrS7PMCcGrY1IE&export=download",
+  "https://drive.google.com/u/6/uc?id=1ahsq43K3ZbzoUjOBZJdOkNSLMkQxOlwp&export=download"
+    
+]
+ 
+
+
+
+    
+    
+                
+            
+
+            
